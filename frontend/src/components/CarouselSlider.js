586acfb2529c330f45a96c04cb8fae6e3d6a7f4b
Abayomi Akinfemiwa <olayodepossible@gmail.com>
2023-06-07 01:04:28 +0100

    fetch pet data from API

--- frontend/src/components/CarouselSlider.js
+++ frontend/src/components/CarouselSlider.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Carousel } from "react-responsive-carousel";
 import { useSelector } from "react-redux";
 import { imageBaseUrl } from "../environment";
+import { getData, getPetMallData } from "../adapter/Axio";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const CarouselSlider = ({ isLargeRow = false }) => {
+ 
+  const [petImages, setPetImages] = useState([]);
   const movies = useSelector((state) => state.movie.data);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await getData('https://dog.ceo/api/breed/bulldog/images');
+      console.log('reqim', req.data.message[0])
+      // const pet = await getPetMallData("/users/");
+      // console.log("PET ==> ", pet);
+      const pet = req.data.message.slice(0, 11)
+      setPetImages([...pet]);
+      
+      return req;
+    };
+    fetchData();
+  }, []);
+
+  console.log('reqim - test', petImages[0])
   return (
-    <Carousel showArrows={true} autoPlay infiniteLoop={true}>
-      {movies?.map(
-        (movie) =>
-          ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
-            <div style={styles.carosel_container} key={movie.id}>
+    <Carousel  autoPlay infiniteLoop={true}>
+      {petImages?.map(
+        (imageUrl, i) =>
+          
+            <div style={styles.carosel_container} key={i}>
               <img
                 style={{ objectFit: "contain", height: "100%" }}
-                src={`${imageBaseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                alt={movie.name}
+                src={imageUrl}
+                alt="pet image"
               />
             </div>
-          )
+        
       )}
     </Carousel>
   );
